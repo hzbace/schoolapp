1cf80b6b9f255bb4135d328b5315c4984cd35631
muye <2470479769@qq.com>
2019-01-22 20:03:51 +0800

    增加修改个人详情接口

--- src/main/java/com/qfedu/schoolapp/SchoolappApplication.java
+++ src/main/java/com/qfedu/schoolapp/SchoolappApplication.java
@@ -1,9 +1,21 @@
 package com.qfedu.schoolapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @EnableSwagger2
 @ImportResource("classpath:ncustomer.xml")
@@ -14,5 +26,21 @@ public class SchoolappApplication {
         SpringApplication.run(SchoolappApplication.class, args);
     }
 
+    @Bean
+    public Converter<String, Date> addNewConvert() {
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(String source) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = null;
+                try {
+                    date = sdf.parse( source);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return date;
+            }
+        };
+    }
 }
 
--- src/main/java/com/qfedu/schoolapp/userdetail/UserDetailController.java
+++ src/main/java/com/qfedu/schoolapp/userdetail/UserDetailController.java
@@ -1,6 +1,6 @@
 package com.qfedu.schoolapp.userdetail;
 
-import com.qfedu.dao.sudetail.SUdetailMapper;
+
 import com.qfedu.entity.sudetail.SUdetail;
 import com.qfedu.service.sudetail.ISUdetailService;
 import com.qfedu.tool.vo.ResultVo;
@@ -10,21 +10,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.Date;
 
 @RestController
 @Api(value = "用户登录,用户退出，审核账号")
 public class UserDetailController {
 
     @Autowired
-    ISUdetailService service;
+    private ISUdetailService service;
 
     @PostMapping("updateUserDetail.do")
     @CrossOrigin
     public ResultVo updateUserDetail(SUdetail sUdetail){
-        return ResultVo.setOK(service.updateById(sUdetail));
+        return service.updateUserDetail(sUdetail);
     }
-
 
 }
